38955ad9eb6e90f155756f9d7b08c458cf52661b
Yuki Tawara <yktawara@gmail.com>
2018-02-26 01:17:00 +0900

    Implement OneBitMinHash

---
+++ src/main/java/com/example/mikarru/minhash/OneBitMinHash.java
@@ -0,0 +1,112 @@
+package com.example.mikkaru.minhash;
+
+import java.io.Serializable;
+import java.util.Random;
+
+import net.openhft.hashing.LongHashFunction;
+
+public class OneBitMinHash {
+  public static class Signature implements Serializable {
+    private int k;
+    private long[] bitVec;
+    private int featureSize;
+
+    Signature(int k, long[] bitVec, int featureSize) {
+      this.k = k;
+      this.bitVec = bitVec;
+      this.featureSize = featureSize;
+    }
+
+    public double calculateSimilarity(Signature that) {
+      if (getK() != that.getK()) {
+        String msg = "Incomportable signatures: this.k = " +
+          getK() + ", that.k = " + that.getK();
+        throw new IllegalArgumentException(msg);
+      }
+
+      if (getFeatureSize() == 0 ^ that.getFeatureSize() == 0) {
+        return 0.0;
+      }
+      if (getFeatureSize() == 0 && that.getFeatureSize() == 0) {
+        return 1.0;
+      }
+
+      double r1 = (double)      getFeatureSize() / Integer.MAX_VALUE;
+      double r2 = (double) that.getFeatureSize() / Integer.MAX_VALUE;
+
+      double a1 = (1.0 - r1) / (2.0 - r1);
+      double a2 = (1.0 - r2) / (2.0 - r2);
+
+      double r1Div = r1 / (r1 + r2);
+      double r2Div = r2 / (r1 + r2);
+
+      double c1 = a1 * r2Div + a2 * r1Div;
+      double c2 = a1 * r1Div + a2 * r2Div;
+
+      double p = calculateP(that);
+      return (p - c1) / (1.0 - c2);
+    }
+
+    private double calculateP(Signature that) {
+      int matched = 0;
+      for (int i = 0; i < bitVec.length; ++i) {
+        long xor = bitVec[i] ^ that.bitVec[i];
+        matched += (64 - Long.bitCount(xor));
+      }
+      return (double) matched / k;
+    }
+
+    public int getK() {
+      return k;
+    }
+
+    public int getFeatureSize() {
+      return featureSize;
+    }
+  }
+
+  public OneBitMinHash(int k) {
+    this(k, 0);
+  }
+
+  private int k;
+  private int arraySize;
+  private LongHashFunction[] hashFuncs;
+
+  public OneBitMinHash(int k, long hashSeed) {
+    if (k <= 0 || k % 64 != 0) {
+      throw new IllegalArgumentException("k must be positive and multiples of 64");
+    }
+    this.k = k;
+
+    this.arraySize = k / 64;
+
+    Random r = new Random(hashSeed);
+    hashFuncs = new LongHashFunction[k];
+    for (int i = 0; i < k; ++i) {
+      hashFuncs[i] = LongHashFunction.murmur_3(r.nextLong());
+    }
+  }
+
+  public Signature calculateSigneture(int[] features) {
+    long[] bitVec = new long[arraySize];
+    int arrIndex = 0;
+    int bitIndex = 0;
+    for (int i = 0; i < k; ++i) {
+      long min = Long.MAX_VALUE;
+      for (int j = 0; j < features.length; ++j) {
+        long hash = hashFuncs[i].hashInt(features[j]);
+        if (hash < min) {
+          min = hash;
+        }
+      }
+      bitVec[arrIndex] |= ((min & 0x1L) << bitIndex);
+      bitIndex++;
+      if (bitIndex == 64) {
+        arrIndex++;
+        bitIndex = 0;
+      }
+    }
+    return new Signature(k, bitVec, features.length);
+  }
+}
